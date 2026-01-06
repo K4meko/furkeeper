0f58838bc2a542a4b4447bbb9f7ab5fa35a90ead
K4meko <pavlueliss@gmail.com>
2026-01-06 13:29:07 +0100

    Stop tracking ignored files

--- README.md
+++
@@ -1,3 +0,0 @@
-# furkeeper
-
-A new Flutter project.
--- lib/models/user.dart
+++
@@ -1,7 +0,0 @@
-class User {
-  late String username;
-  late String email;
-  late String? token;
-
-  User({required this.username, required this.email});
-}
